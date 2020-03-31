800686a9c59b8b0314636784875a01447db4fc78
Seiwell <57401916+Seiwell0610@users.noreply.github.com>
2020-03-31 11:36:22 +0900

    Add files via upload

--- cogs/info.py
+++ cogs/info.py
@@ -7,8 +7,8 @@ class Help(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name='info', pass_context=True, hidden=True)
-    async def helps(self, ctx):
+    @commands.command()
+    async def info(self, ctx):
         invite = "https://discordapp.com/api/oauth2/authorize?client_id=688553944661754054&permissions=379968&scope=bot"
         url = "https://discord.gg/25yrUVp"
         timestamp = datetime.utcfromtimestamp(int(self.bot.user.created_at.timestamp()))
@@ -22,11 +22,15 @@ class Help(commands.Cog):
         embed.add_field(name="応答速度", value=f"`{self.bot.ws.latency * 1000:.0f}ms`")
         embed.set_footer(text="このBOTの作成日")
         return await ctx.send(embed=embed)
-        ### データを送信 ###
-        channel = self.bot.get_channel(676716174087684126)
-        embed = discord.Embed(title=f"コマンド実行", description=f"{message.author.mention}\n`{p}info`コマンドの実行",
-                              color=discord.Color.blue())
-        await channel.send(embed=embed)
+
+    @commands.command()
+    async def help(self, ctx):
+        embed = discord.Embed(title="このBOTで使用できる機能:", color=0x942192)
+        embed.add_field(name="[コマンド] ;info", value="招待リンクや応答速度などこのBOTの概要について表示します。", inline=False)
+        embed.add_field(name="[コマンド] ;cs", value="指定されたキャラクターの主なステータスとwikiへのリンクを表示します。", inline=False)
+        embed.add_field(name="[機能] グローバルチャット",
+                        value="他のサーバーにこのBOTを入れ、「arknights-global」というチャンネル名で\n作成すると、グローバルチャットが使用できます", inline=False)
+        return await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Help(bot))
