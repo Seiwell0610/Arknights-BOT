110ba8638ac7baf64e0f2086fb240f2d40804f78
Seiwell <57401916+Seiwell0610@users.noreply.github.com>
2020-04-06 15:15:19 +0900

    Update dropbox.py

--- cogs/dropbox.py
+++ cogs/dropbox.py
@@ -11,7 +11,7 @@ class dropbox(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @tasks.loop(seconds=15)
     async def upload(self, ctx):
         print("アップロード処理を開始")
         with open("data.csv", "rb") as f:
