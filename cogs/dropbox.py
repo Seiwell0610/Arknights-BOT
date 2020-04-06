6f307a11f8efe6e977be1e21176d1a1bd5fe9ea2
Seiwell <57401916+Seiwell0610@users.noreply.github.com>
2020-04-06 14:53:07 +0900

    Update dropbox.py

--- cogs/dropbox.py
+++ cogs/dropbox.py
@@ -16,7 +16,7 @@ class dropbox(commands.Cog):
         print("アップロード処理を開始")
         with open("data.csv", "rb") as f:
             print("ファイルの読み込み完了")
-            dbx.files_upload(f.read(), UPLOADPATH_DBX, mode=dropbox.files.WriteMode.overwrite)
+            dbx.files_upload(f.read(),UPLOADPATH_DBX,mode=dropbox.files.WriteMode.overwrite)
             print("アップロード処理完了")
 
 def setup(bot):
