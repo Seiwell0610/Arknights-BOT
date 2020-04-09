d02f5e38dffc1a2ef55a0444fa0997e93db7f54b
Seiwell <57401916+Seiwell0610@users.noreply.github.com>
2020-04-09 22:07:47 +0900

    Add files via upload

--- cogs/dropbox.py
+++ cogs/dropbox.py
@@ -4,8 +4,6 @@ from discord.ext import tasks, commands
 dbxtoken = "_Qobiq7UxdAAAAAAAAAAVwmGwxNRDjQuXNSmgwP6N8dqq9umopY2xvaDsc1saAJJ"
 dbx = dropbox.Dropbox(dbxtoken)
 dbx.users_get_current_account()
-UPLOADPATH_LOCAL = "data.csv"
-UPLOADPATH_DBX = "/data.csv"
 
 class dropbox(commands.Cog):
     def __init__(self, bot):
@@ -14,10 +12,17 @@ class dropbox(commands.Cog):
 
     @tasks.loop(seconds=30)
     async def upload(self):
-        print("アップロード処理開始")
-        with open(UPLOADPATH_LOCAL, "rb") as f:
-            dbx.files_upload(f.read(), UPLOADPATH_DBX)
-        print("アップロード処理完了")
+        print("処理開始(data.csv)")
+        with open("data.csv", "rb") as f:
+            dbx.files_upload(f.read(), "/data.csv")
+        print("アップロード完了(data.csv)")
+
+    @tasks.loop(seconds=30)
+    async def upload(self):
+        print("処理開始(channel_id)")
+        with open("channel_id.txt", "rb") as f:
+            dbx.files_upload(f.read(), "/channel_id.txt")
+        print("アップロード完了(channel_id)")
 
 def setup(bot):
     bot.add_cog(dropbox(bot))
