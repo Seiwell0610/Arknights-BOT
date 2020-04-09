7bc68b0462d4e515180b3f414841c4ff183a1c10
Seiwell <57401916+Seiwell0610@users.noreply.github.com>
2020-04-09 22:47:36 +0900

    Add files via upload

--- cogs/main_cog.py
+++ cogs/main_cog.py
@@ -38,7 +38,7 @@ class Member(commands.Cog):
                 await message.channel.send(embed=embed)
 
         if message.content.startswith(f"{p}add_global"):
-            ch_id = message.channel.id + ", "
+            ch_id = (str(message.channel.id)) + ", "
             with open("channel_id.txt", mode="a") as f:
                 f.write(ch_id)
             await message.send(f"{message.author.mention}-> グローバルチャットに追加しました。 ")
