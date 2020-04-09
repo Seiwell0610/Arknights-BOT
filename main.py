be74f970151df7fff28a3545ba640ab6d88b14bc
Seiwell <57401916+Seiwell0610@users.noreply.github.com>
2020-04-09 22:28:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,6 @@ loop = asyncio.new_event_loop()
 dbxtoken = "_Qobiq7UxdAAAAAAAAAAUSQMe2MDJyrmNyMWglSKGrfZKrrzGx_ruooafYposH3L"
 dbx = dropbox.Dropbox(dbxtoken)
 dbx.users_get_current_account()
-UPLOADPATH_LOCAL = "data.csv"
-UPLOADPATH_DBX = "/data.csv"
 
 async def run():
     bot = MyBot()
@@ -32,7 +30,7 @@ class MyBot(commands.Bot):
         self.remove_command('help')
 
     async def on_ready(self):
-        for extension in ["info", "main_cog", "sub_cog", "global_chat", "eval", "dropbox"]:
+        for extension in ["info", "main_cog", "sub_cog", "global_chat", "eval"]:
             try:
                 self.load_extension(f"cogs.{extension}")
             except commands.ExtensionAlreadyLoaded:
