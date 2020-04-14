43669622af22aac0de30a84a6205208dcbbb8ef1
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-04-14 16:08:49 +0900

    Update global_chat.py

--- cogs/global_chat.py
+++ cogs/global_chat.py
@@ -60,7 +60,7 @@ class arknights_global(commands.Cog):
                                          icon_url=message.guild.icon_url_as(format="png"))
 
                     for p in message.attachments:
-                        await self.bot.get_channel(698357464973967360).send(p[3])
+                        print(p[3])
                         #embed.set_image(url=p['url'])
 
                 for channel in global_channels:
