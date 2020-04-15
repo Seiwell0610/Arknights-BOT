7a68643dbd888c07e7cd15fded38c438c1f5674f
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-04-15 22:00:32 +0900

    Update global_chat.py

--- cogs/global_chat.py
+++ cogs/global_chat.py
@@ -5,12 +5,6 @@ import asyncio
 import requests
 import datetime
 
-def download_img(url, file_name):
-    r = requests.get(url, stream=True)
-    if r.status_code == 200:
-        with open(file_name, 'wb') as f:
-            f.write(r.content)
-
 class arknights_global(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
