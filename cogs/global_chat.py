1b0519d61825b4fc2f62232fec370f3b6cc1c72d
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-04-15 10:19:38 +0900

    Update global_chat.py

--- cogs/global_chat.py
+++ cogs/global_chat.py
@@ -27,7 +27,10 @@ class arknights_global(commands.Cog):
 
                 if not message.attachments:
                     await message.delete()
-
+                else:
+                    filepath = message.attachments[0].url
+                    channel = self.bot.get_channel(697704665818726501)
+                    await self.bot.send_file(channel,filepath)
                 channels = self.bot.get_all_channels()
                 global_channels = [ch for ch in channels if ch.id in GLOBAL_CH_ID]
 
