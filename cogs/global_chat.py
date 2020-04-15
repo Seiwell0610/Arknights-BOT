0471658b5a7c608a7276f00914c891b583258d9c
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-04-16 00:05:48 +0900

    Update global_chat.py

--- cogs/global_chat.py
+++ cogs/global_chat.py
@@ -49,6 +49,7 @@ class arknights_global(commands.Cog):
                                            username=message.author.name,
                                            avatar_url=message.author.avatar_url_as(format="png"))
                         return
- 
+
+                await message.channel.send("line31 Error")
 def setup(bot):
     bot.add_cog(arknights_global(bot))
