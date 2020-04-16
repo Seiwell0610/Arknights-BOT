d375b06347748b26c6a85af1eab9d8dc77e5ceaf
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-04-16 16:37:14 +0900

    Update global_chat.py

--- cogs/global_chat.py
+++ cogs/global_chat.py
@@ -21,6 +21,7 @@ class arknights_global(commands.Cog):
         for row in c.execute("SELECT * FROM global_chat"):
             GLOBAL_CH_ID.append(row[0])
 
+        print(GLOBAL_CH_ID)
         if message.channel.id in GLOBAL_CH_ID:
 
             if message.content.startswith(";"):
