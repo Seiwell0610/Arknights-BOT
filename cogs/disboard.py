323bb0710627c504c7041ca4c34f6c86ca14eca8
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-05-06 15:43:17 +0900

    Update disboard.py

--- cogs/disboard.py
+++ cogs/disboard.py
@@ -1,1 +1,18 @@
+import discord
+from discord.ext import commands
+import asyncio
 
+class Disboard(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if ctx.author.id == 302050872383242240 :
+            if "表示順をアップ" in ctx.embeds[0].description:
+                await ctx.send("Bumpを確認しました。2時間後に通知します。")
+                await asyncio.sleep(2*60*60)
+                await ctx.send("Bump可能通知")
+            
+def setup(bot):
+    bot.add_cog(Disboard(bot))
