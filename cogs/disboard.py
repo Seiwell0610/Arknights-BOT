b1f22748e89fed06beb7a7ba13196de8aa2f68f0
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-05-07 15:08:38 +0900

    Update disboard.py

--- cogs/disboard.py
+++ cogs/disboard.py
@@ -10,7 +10,8 @@ class Disboard(commands.Cog):
     async def on_message(self, message):
         if message.author.id == 302050872383242240 :
             if "表示順をアップ" in message.embeds[0].description:
-                msg = await message.channel.send("Bumpを確認しました。2時間後に通知します。")
+                mn = message.author.name
+                msg = await message.channel.send(f"{mn}さんBumpを確認しました。\n2時間後に通知します。")
                 await asyncio.sleep(10)
                 m = 0
                 while m < 7201:
@@ -19,7 +20,7 @@ class Disboard(commands.Cog):
                     await msg.edit(content=f"あと{s}秒後にBumpできます。")
                     await asyncio.sleep(1)
                     m += 1
-                await msg.edit(content=f"Bump可能")
+                await msg.edit(content=f"<@&694401914770554881>{mn}\nBump可能")
             
 def setup(bot):
     bot.add_cog(Disboard(bot))
