3c1c73682b7e1f001660ca3b1744dbb02febc7c1
Seiwell <57401916+Seiwell0610@users.noreply.github.com>
2020-05-19 10:38:29 +0900

    Add files via upload

---
+++ cogs/global_chat.py
@@ -0,0 +1,69 @@
+import discord
+import dropbox
+import sqlite3
+from discord.ext import commands
+
+dbxtoken = "_Qobiq7UxdAAAAAAAAAAVwmGwxNRDjQuXNSmgwP6N8dqq9umopY2xvaDsc1saAJJ"
+dbx = dropbox.Dropbox(dbxtoken)
+dbx.users_get_current_account()
+
+class Member(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    ### グローバルチャット関連 ###
+    @commands.command(name="add_global")
+    @commands.has_permissions(manage_guild=True)
+    async def _add_global(self, ctx):
+        channel = self.bot.get_channel(698520936374075453)
+        ch_id = ctx.channel.id
+        ch_name = ctx.channel.name
+        guild = ctx.guild.name
+        guild_id = ctx.guild.id
+
+        conn = sqlite3.connect('all_data.db')
+        c = conn.cursor()
+        c.execute("CREATE TABLE IF NOT EXISTS global_chat(id integer PRIMARY KEY, name text NOT NULL)")
+        c.execute("insert into global_chat values(?,?)", (ch_id, ch_name))
+        conn.commit()
+        conn.close()
+        await ctx.send(f"{ctx.author.mention}-> グローバルチャットに登録しました。")
+
+        with open("all_data.db", "rb") as fc:
+            dbx.files_upload(fc.read(), "/all_data.db", mode=dropbox.files.WriteMode.overwrite)
+
+        embed = discord.Embed(title="グローバルチャット[登録]", description=None, color=discord.Color.blue())
+        embed.add_field(name=f"GUILD", value=f"{guild}", inline=False)
+        embed.add_field(name="GUILD ID", value=f"{guild_id}", inline=False)
+        embed.add_field(name="CHANNEL", value=f"{ch_name}", inline=False)
+        embed.add_field(name="CHANNEL ID", value=f"{ch_id}", inline=False)
+        await channel.send(embed=embed)
+
+    @commands.command(name="del_global")
+    @commands.has_permissions(manage_guild=True)
+    async def _del_global(self, ctx):
+        channel = self.bot.get_channel(698520936374075453)
+        ch_id = ctx.channel.id
+        ch_name = ctx.channel.name
+        guild = ctx.guild.name
+        guild_id = ctx.guild.id
+
+        conn = sqlite3.connect("all_data.db")
+        c = conn.cursor()
+        c.execute('DELETE FROM global_chat WHERE id = ?', (ch_id,))
+        conn.commit()
+        conn.close()
+        await ctx.send(f"{ctx.author.mention}-> グローバルチャットの登録を解除しました。")
+
+        with open("all_data.db", "rb") as fc:
+            dbx.files_upload(fc.read(), "/all_data.db", mode=dropbox.files.WriteMode.overwrite)
+
+        embed = discord.Embed(title="グローバルチャット[解除]", description=None, color=discord.Color.dark_red())
+        embed.add_field(name=f"GUILD", value=f"{guild}", inline=False)
+        embed.add_field(name="GUILD ID", value=f"{guild_id}", inline=False)
+        embed.add_field(name="CHANNEL", value=f"{ch_name}", inline=False)
+        embed.add_field(name="CHANNEL ID", value=f"{ch_id}", inline=False)
+        await channel.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(Member(bot))
