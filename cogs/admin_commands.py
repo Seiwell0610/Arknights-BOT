fa145c24ee3ce0e2e2cdfd3da43c5314031b66a3
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-05-22 11:48:56 +0900

    Update admin_commands.py

--- cogs/admin_commands.py
+++ cogs/admin_commands.py
@@ -29,7 +29,7 @@ class Member(commands.Cog):
                 embed = discord.Embed(title=f"{title}", description=f"{main}")
                 channel = self.bot.get_channel(int(channel_id))
                 await channel.send(embed=embed)
-                if channel_id!=None:
+                if channel_id!=ctx.channel.id:
                     await ctx.send(f"{ctx.author.mention}-> メッセージの送信が完了しました。")
 
             except:
