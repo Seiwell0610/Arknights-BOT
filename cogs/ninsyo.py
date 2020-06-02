b58ce3421105ed20ae199e8f29f7ec2ea190d3e0
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-06-02 17:38:32 +0900

    Update ninsyo.py

--- cogs/ninsyo.py
+++ cogs/ninsyo.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import discord
 import datetime
+import r
 
 main_guild_id=689263691669176426
 
@@ -8,20 +9,28 @@ class Ninsyo(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="")
+    @commands.command(name="arknight")
     async def _ninsyo(self, ctx):
         if ctx.guild.id!=main_guild_id:
             return
         command_time=datetime.datetime.now()
         command_minute=command_time.minute
+        members=ctx.author.id
         #データベース読み込み(値をjtに)
+        conn=r.connect()
+        jt=conn.get(members)
+        jt=str(jt)
         x=command_minute-jt
         if x<0:
             x+=60
         if x=>5:
-            True
+            #ここに
+            await ctx.send("登録しました")
+            p=conn.delete(members)
+            print(p)
         else:
-            False
+            x=5-x
+            await ctx.send(f"後{x}分後に登録できます")
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
@@ -32,6 +41,9 @@ class Ninsyo(commands.Cog):
         join_time=datetime.datetime.now()
         join_minute=join_time.minute
         member_id=member.id
+        conn=r.connect()
+        ps=conn.set(member_id,join_minute)
+        print(ps)
         #データベース書き込み(keyをmember_id,valueをjoin_minute)
 
 def setup(bot):
