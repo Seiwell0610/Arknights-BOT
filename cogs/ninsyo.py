0b52f99146601f507aa31de264777a9643b3121c
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-06-02 15:58:57 +0900

    Update ninsyo.py

--- cogs/ninsyo.py
+++ cogs/ninsyo.py
@@ -6,5 +6,13 @@ class Ninsyo(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command()
+    async def _ninsyo(self, ctx):
+        
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        
+
 def setup(bot):
     bot.add_cog(Ninsyo(bot))
