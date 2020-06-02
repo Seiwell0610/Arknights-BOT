dae88697091deb4cf45a4fde0c1bb62c804b52d2
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-06-02 15:51:23 +0900

    Update ninsyo.py

--- cogs/ninsyo.py
+++ cogs/ninsyo.py
@@ -1,1 +1,10 @@
+from discord.ext import commands
+import discord
+import datetime
 
+class Ninsyo(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+def setup(bot):
+    bot.add_cog(Ninsyo(bot))
