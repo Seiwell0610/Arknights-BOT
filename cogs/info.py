10eb722fedaa06bc980e85ccade0c6119397cc79
Seiwell <57401916+Seiwell0610@users.noreply.github.com>
2020-06-23 08:28:34 +0900

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -98,7 +98,7 @@ class Help(commands.Cog):
             nav.start()
             await ctx.send(nav)
             
-    @help.group(name="mainte")
+    @help.command(name="mainte")
     async def _mainte(self, ctx)
         embed=discord.Embed(title="`;meinte <モード>`のヘルプ", description="0：通常モード\n1：グローバルチャット以外停止\n2：グローバルチャットを含め停止\n3：グローバルチャットのみ停止", color=discord.Color.blue())
         await ctx.send(embed=embed)
