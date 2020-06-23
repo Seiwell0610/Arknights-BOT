c93ddeef08c5205c0807488600c2b53e6878a847
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-06-23 14:52:31 +0900

    Update global_chat.py

--- cogs/global_chat.py
+++ cogs/global_chat.py
@@ -23,7 +23,7 @@ class Member(commands.Cog):
             conn=r.connect()
             pp=conn.get("maintenance")
             pp=int(pp)
-            q = ['0','3']
+            q = ['0','1']
             if pp not in q:
                 return await ctx.send("現在、メンテナンス中です")
 
