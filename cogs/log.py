497bc7d424ecca8dc63b78fdf1d9c5acbe9b1c7e
Seiwell <57401916+Seiwell0610@users.noreply.github.com>
2020-07-11 19:23:57 +0900

    Add files via upload

--- cogs/log.py
+++ cogs/log.py
@@ -21,5 +21,18 @@ class log(commands.Cog):
         embed = discord.Embed(title="脱退", description=f"`{guild}`に脱退しました。", color=discord.Color.dark_red())
         await channel.send(embed=embed)
 
+    @commands.Cog.listener()
+    async def on_command(self, ctx):
+        ch = self.bot.get_channel(714615013968576572)
+        url = ctx.author.avatar_url_as(format=None, static_format='png', size=1024)
+        embed = discord.Embed(title="コマンドログ", description="", color=0x00fa9a)
+        embed.add_field(name="コマンド実行者", value=f"{ctx.author.name}(ID:{ctx.author.id})", inline=False)
+        embed.add_field(name="実行したコマンド", value=f"{ctx.command.name}")
+        embed.add_field(name="サーバー", value=f"{ctx.guild.name}(ID:{ctx.guild.id})")
+        embed.add_field(name="実行文", value=f"{ctx.message.content}")
+        embed.set_thumbnail(url=url)
+        embed.timestamp = ctx.message.created_at
+        await ch.send(embed=embed)
+
 def setup(bot):
     bot.add_cog(log(bot))
