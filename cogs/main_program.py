f37df2a672b6d11269de7a75dd788c8cbc958e6d
Seiwell <57401916+Seiwell0610@users.noreply.github.com>
2020-07-11 16:23:35 +0900

    Add files via upload

--- cogs/main_program.py
+++ cogs/main_program.py
@@ -9,13 +9,6 @@ print("main_programの読み込み完了")
 
 admin_list = admin.admin_list
 
-#データベース読み込み
-conn = sqlite3.connect("all_data_arknights_main.db")
-c = conn.cursor()
-
-def list_split(count, db_data):
-        return db_data[count].split("/")
-
 def default_buttons():
     from libneko.pag.reactionbuttons import (
         first_page,
@@ -45,7 +38,7 @@ class main_program(commands.Cog):
         if ctx.author.id not in admin_list:
             conn = r.connect()
             pp = conn.get("maintenance")
-            q = ['0', '3']
+            q = ['0','3']
             if pp not in q:
                 embed = discord.Embed(title="メンテナンス中", description="現在、メンテナンス中のため使用できません。\nメンテナンスが終わるまでお待ちください。",
                                       color=discord.Color.dark_red())
@@ -75,6 +68,8 @@ class main_program(commands.Cog):
             return await ctx.send(embed=embed)
 
         character.title()
+        conn = sqlite3.connect("all_data_arknights_main.db")
+        c = conn.cursor()
         c.execute('SELECT * FROM character WHERE 名前=?', (character,))
         data = c.fetchone()
 
@@ -84,19 +79,27 @@ class main_program(commands.Cog):
             return await ctx.send(embed=embed)
 
         else:
+            promotion = data[3].split("/")
+            hp = data[4].split("/")  # HP
+            attack = data[5].split("/")  # 攻撃力
+            defense = data[6].split("/")  # 防御力
+            magic_defense = data[7].split("/")  # 術耐性
+            cost = data[9].split("/")  # 配置コスト
+            block = data[10].split("/")  # ブロック数
+
             pages = []
 
-            for count in range(int(len(list_split(3, db_data=data)))):
-                pages.append(discord.Embed(title=f"{data[0]}({list_split(3, db_data=data)[count]})", color=0x0096ff))
+            for count in range(int(len(promotion))):
+                pages.append(discord.Embed(title=f"{data[0]}({promotion[count]})", color=0x0096ff))
                 pages[count].add_field(name="職業", value=f"{data[1]}")
                 pages[count].add_field(name="レア度", value=f"{data[2]}")
-                pages[count].add_field(name="HP(信頼度)", value=f"{list_split(4, db_data=data)[count]}")
-                pages[count].add_field(name="攻撃力(信頼度)", value=f"{list_split(5, db_data=data)[count]}")
-                pages[count].add_field(name="防御力(信頼度)", value=f"{list_split(6, db_data=data)[count]}")
-                pages[count].add_field(name="術耐性", value=f"{list_split(7, db_data=data)[count]}")
+                pages[count].add_field(name="HP(信頼度)", value=f"{hp[count]}")
+                pages[count].add_field(name="攻撃力(信頼度)", value=f"{attack[count]}")
+                pages[count].add_field(name="防御力(信頼度)", value=f"{defense[count]}")
+                pages[count].add_field(name="術耐性", value=f"{magic_defense[count]}")
                 pages[count].add_field(name="再配置速度", value=f"{data[8]}")
-                pages[count].add_field(name="配置コスト", value=f"{list_split(8, db_data=data)[count]}")
-                pages[count].add_field(name="ブロック数", value=f"{list_split(9, db_data=data)[count]}")
+                pages[count].add_field(name="配置コスト", value=f"{cost[count]}")
+                pages[count].add_field(name="ブロック数", value=f"{block[count]}")
                 pages[count].add_field(name="攻撃速度", value=f"{data[11]}")
                 pages[count].add_field(name="募集タグ", value=f"{data[12]}")
                 pages[count].add_field(name="リンク", value=f"[詳細はこちら](<{data[13]}>)", inline=True)
@@ -119,7 +122,6 @@ class main_program(commands.Cog):
                 return await ctx.send(embed=embed)
 
         channel = self.bot.get_channel(714615013968576572)
-
         """コマンドログを送信"""
         embed = discord.Embed(title="コマンド実行ログ", color=discord.Color.orange())
         embed.set_thumbnail(url=ctx.author.avatar_url_as(format="png"))
@@ -136,8 +138,8 @@ class main_program(commands.Cog):
         embed.add_field(name='\u200b', value='\u200b')
         embed.add_field(name="チャンネルID", value=f"{ctx.channel.id}", inline=True)
         await channel.send(embed=embed)
-
-        #データベース読み込み
+        conn = sqlite3.connect("all_data_arknights_main.db")
+        c = conn.cursor()
         c.execute('SELECT * FROM character_skill WHERE 名前=?', (character,))
         data = c.fetchone()
 
@@ -153,6 +155,15 @@ class main_program(commands.Cog):
 
         else:
 
+            quality_name = data[2].split("/")
+            quality = data[3].split("/")
+            skill_name = data[4].split("/")
+            skill = data[5].split("/")
+            base_skill_name = data[6].split("/")
+            base_skill_conditions = data[7].split("/")
+            base_skill_target = data[8].split("/")
+            base_skill_effect = data[9].split("/")
+
             pages = [
                 (discord.Embed(title=f"{character}", color=discord.Color.orange())),
                 (discord.Embed(title=f"{character}(素質)", color=discord.Color.orange())),
@@ -162,16 +173,16 @@ class main_program(commands.Cog):
 
             pages[0].add_field(name=f"特性", value=f"{data[1]}\n", inline=False)
 
-            for count in range(int(len(list_split(2, db_data=data)))):
-                pages[1].add_field(name=f"{list_split(2, db_data=data)[count]}", value=f"{list_split(3, db_data=data)[count]}\n", inline=False)
+            for count in range(int(len(quality_name))):
+                pages[1].add_field(name=f"{quality_name[count]}", value=f"{quality[count]}\n", inline=False)
 
-            for count in range(int(len(list_split(4, db_data=data)))):
-                pages[2].add_field(name=f"{list_split(4, db_data=data)[count]}", value=f"{list_split(5, db_data=data)[count]}", inline=False)
+            for count in range(int(len(skill_name))):
+                pages[2].add_field(name=f"{skill_name[count]}", value=f"{skill[count]}", inline=False)
 
-            for count in range(int(len(list_split(6, db_data=data)))):
-                pages[3].add_field(name=f"{list_split(6, db_data=data)[count]}", value=f"{list_split(9, db_data=data)[count]}", inline=False)
-                pages[3].add_field(name=f"習得条件", value=f"{list_split(7, db_data=data)[count]}", inline=True)
-                pages[3].add_field(name=f"効果対象", value=f"{8[count]}", inline=True)
+            for count in range(int(len(base_skill_name))):
+                pages[3].add_field(name=f"{base_skill_name[count]}", value=f"{base_skill_effect[count]}", inline=False)
+                pages[3].add_field(name=f"習得条件", value=f"{base_skill_conditions[count]}", inline=True)
+                pages[3].add_field(name=f"効果対象", value=f"{base_skill_target[count]}", inline=True)
 
             nav = libneko.pag.navigator.EmbedNavigator(ctx, pages, buttons=default_buttons(), timeout=10)
             nav.start()
