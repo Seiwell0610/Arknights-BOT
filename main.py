e29967b63b0ab8e6dbb919fd66a4639e0fe748b3
Seiwell <57401916+Seiwell0610@users.noreply.github.com>
2020-07-11 19:14:44 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,52 +1,41 @@
-import json
 import discord
-import dropbox
-import asyncio
-import traceback
 from discord.ext import commands
+import json
+import dropbox
+import traceback
 
 with open('setting.json', mode='r', encoding='utf-8') as fh:
     json_txt = fh.read()
     json_txt = str(json_txt).replace("'", '"').replace('True', 'true').replace('False', 'false')
     token = json.loads(json_txt)['token']
     prefix = json.loads(json_txt)['prefix']
-loop = asyncio.new_event_loop()
 
 dbxtoken = "_Qobiq7UxdAAAAAAAAAAUSQMe2MDJyrmNyMWglSKGrfZKrrzGx_ruooafYposH3L"
 dbx = dropbox.Dropbox(dbxtoken)
 dbx.users_get_current_account()
 
-async def run():
-    bot = MyBot()
-    try:
-        await bot.start(token)
-    except KeyboardInterrupt:
-        await bot.logout()
-
 class MyBot(commands.Bot):
     def __init__(self, command_prefix):
-        super().__init__(command_prefix, loop=loop)
-        self.remove_command('help')
+        super().__init__(command_prefix)
 
-    async def on_ready(self):
-        for extension in ["admin", "help", "url", "log", "global_chat", "main_program", "disboard", "limited", "auxiliary"]:
+        for extension in ["admin", "help", "url", "log", "global_chat", "main_program", "disboard", "limited","auxiliary"]:
             try:
                 self.load_extension(f"cogs.{extension}")
             except commands.ExtensionAlreadyLoaded:
                 self.reload_extension(f"cogs.{extension}")
 
-
-        await self.change_presence(activity=discord.Game(name=f"{prefix}help | {len(self.guilds)}guilds"))
+        await self.change_presence(activity=discord.Game(name=f"{prefix}about | {len(self.guilds)}guilds"))
 
     async def on_guild_join(self, _):
-        await self.change_presence(activity=discord.Game(name=f"{prefix}help | {len(self.guilds)}guilds"))
+        await self.change_presence(activity=discord.Game(name=f"{prefix}about | {len(self.guilds)}guilds"))
 
     async def on_guild_remove(self, _):
-        await self.change_presence(activity=discord.Game(name=f"{prefix}help | {len(self.guilds)}guilds"))
+        await self.change_presence(activity=discord.Game(name=f"{prefix}about | {len(self.guilds)}guilds"))
 
     async def on_command_error(self, ctx, error1):
         if isinstance(error1, (commands.CommandNotFound, commands.CommandInvokeError)):
             return
+
 
 if __name__ == '__main__':
     try:
@@ -56,10 +45,8 @@ if __name__ == '__main__':
             f.write(res.content)
         print("データベースのダウンロード完了")
 
-        main_task = loop.create_task(run())
-        loop.run_until_complete(main_task)
-        loop.close()
-        bot = MyBot(command_prefix=prefix)
+        bot = MyBot(command_prefix=f'{prefix}')
+        bot.run(token)
 
         @bot.event
         async def on_command(ctx):
