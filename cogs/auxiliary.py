41fd40fac833321e952a730b1c73277ce19690c3
Seiwell <57401916+Seiwell0610@users.noreply.github.com>
2020-07-13 08:09:39 +0900

    Update auxiliary.py

--- cogs/auxiliary.py
+++ cogs/auxiliary.py
@@ -14,10 +14,41 @@ class auciliary(commands.Cog):
             await ctx.channel.purge(limit=limit)
             embed = discord.Embed(title="メッセージ削除", description=f"{limit}件のメッセージを削除しました。", color=discord.Color.red())
             await ctx.send(embed=embed)
-
+    
         except:
             embed = discord.Embed(title="メーセージの削除失敗", description="メッセージの削除に失敗しました。", color=discord.Color.dark_red())
             await ctx.send(embed=embed)
+    
+    @commands.command()
+    async def report(self, message):
+        #title
+        em1 = discord.Embed(description="タイトルを入力してください",color=0x009193)
+        await message.channel.send(embed=em1)
+        def check1(m):
+            return m.content and m.author == message.author    
+        try:
+            titl = await self.bot.wait_for("message",timeout=30.0, check=check1)
+
+        except asyncio.TimeoutError:
+            return await message.channel.send('タイムアウトしました。')
+
+        #description
+        em2 = discord.Embed(description="報告内容を入力してください",color=0x009193)
+        await message.channel.send(embed=em2)
+        def check2(m):
+            return m.content and m.author == message.author    
+        try:
+            dis = await self.bot.wait_for("message",timeout=30.0, check=check2)
+            #送信
+            embed = discord.Embed(title=str(titl.content),description=str(dis.content),color=0x009193)
+            await self.bot.get_channel(731664672222347295).send(embed=embed)
+            em3 = discord.Embed(description="バグの報告ありがとうございました。\n以下の内容で報告いたしました。",color=0x009193)
+            em3.add_field(name="タイトル", value=f"{titl.content}")
+            em3.add_field(name="内容", value=f"{dis.content}")
+            await ctx.send(embed=em3)
+
+        except asyncio.TimeoutError:
+            return await message.channel.send('タイムアウトしました。')
 
 def setup(bot):
     bot.add_cog(auciliary(bot))
