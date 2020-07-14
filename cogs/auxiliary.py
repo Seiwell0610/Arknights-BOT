a9bd3df4873f3ab9faf0cdc2c56de4048da0c7e9
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-07-14 14:17:12 +0900

    Update auxiliary.py

--- cogs/auxiliary.py
+++ cogs/auxiliary.py
@@ -35,28 +35,34 @@ class auxiliary(commands.Cog):
             return await ctx.send('タイムアウトしました。')
 
         else:
-            #description
-            em2 = discord.Embed(description="報告内容を入力してください",color=0x009193)
-            await ctx.send(embed=em2)
-            def check2(m):
-                return m.content and m.author == message.author    
-            try:
-               dis = await self.bot.wait_for("message",timeout=30.0, check=check2)
-            
-            except asyncio.TimeoutError:
-               return await ctx.send('タイムアウトしました。')
+            if titl.content != None:
+                title = titl.content
 
-            else:
-                #送信
-                em3 = discord.Embed(title=f"**{ctx.author}からのバグレポート**", color=0x009193)
-                em3.add_field(name=f"{titl.content}", value=f"{dis.content}")
-                ch = self.bot.get_channel(int(731664672222347295))
-                await ch.send(embed=em3)
-                em4 = discord.Embed(title="バグの報告ありがとうございました。", description="以下の内容で報告いたしました。",color=0x009193)
-                em4.add_field(name="タイトル", value=f"{titl.content}", inline=False)
-                em4.add_field(name="内容", value=f"{dis.content}", inline=False)
-                await ctx.send(embed=em4)
-                return
+        #description
+        em2 = discord.Embed(description="報告内容を入力してください",color=0x009193)
+        await ctx.send(embed=em2)
+        def check2(m):
+            return m.content and m.author == message.author    
+        try:
+            dis = await self.bot.wait_for("message",timeout=30.0, check=check2)           
+        except asyncio.TimeoutError:
+            return await ctx.send('タイムアウトしました。')
+
+        else:
+            if dis.content != None:
+                dis = dis.content
+        #送信
+        em4 = discord.Embed(title="バグの報告ありがとうございました。", description="以下の内容で報告いたしました。",color=0x009193)
+        em4.add_field(name="タイトル", value=f"{title}", inline=False)
+        em4.add_field(name="内容", value=f"{dis}", inline=False)
+        await ctx.send(embed=em4)
+
+        em3 = discord.Embed(title=f"**{ctx.author}からのバグレポート**", color=0x009193)
+        em3.add_field(name=f"{title}", value=f"{dis}")
+        ch = self.bot.get_channel(int(731664672222347295))
+        await ch.send(embed=em3)
+
+        return
 
 def setup(bot):
     bot.add_cog(auxiliary(bot))
