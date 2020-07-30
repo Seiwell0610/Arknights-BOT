b6052ee8a73e7b16316eab96a29e97c7288be279
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-07-30 17:11:01 +0900

    Update twapi.py

--- cogs/twapi.py
+++ cogs/twapi.py
@@ -1,13 +1,20 @@
 from discord.ext import commands,tasks
 import asyncio
 import traceback
+import tw
 
-print("disboardの読み込み完了")
+print("Twitterの読み込み完了")
 
 class Twitter(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.disb.start()
+
+    @commands.command()
+    async def twitter(self, ctx, id=None):
+        if id == None:
+            id = 'ArknightsStaff'
+        
 
 def setup(bot):
     bot.add_cog(Twitter(bot))
