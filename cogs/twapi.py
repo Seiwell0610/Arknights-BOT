e14789e89764fa33b10f46a89a8c976f3cc4ab68
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-07-30 17:29:17 +0900

    Update twapi.py

--- cogs/twapi.py
+++ cogs/twapi.py
@@ -2,14 +2,18 @@ from discord.ext import commands,tasks
 import asyncio
 import traceback
 import tw
+import tweepy
 
 print("Twitterの読み込み完了")
+
+api = tw.api
 
 class Twitter(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.disb.start()
+        self.twap.start()
 
+    #検索
     @commands.command()
     async def twitter(self, ctx, id=None):
         if id == None:
