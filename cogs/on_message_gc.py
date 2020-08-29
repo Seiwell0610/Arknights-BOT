d6f7aef8af8b50a4d0ca809c69f551132a09ba03
Huyu2239 <61175422+Huyu2239@users.noreply.github.com>
2020-08-29 18:12:30 +0900

    [修正]

--- cogs/on_message_gc.py
+++ cogs/on_message_gc.py
@@ -28,46 +28,116 @@ class on_global_chat(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="add_global")
-    @commands.has_permissions(manage_guild=True)
-    async def _add_global(self, ctx):
-        ch_id = int(ctx.channel.id)
-        guild_id = int(ctx.guild.id)
+    @commands.Cog.listener()
+    async def on_message(self, message):
 
-        c.execute("insert into global_chat (guild_id, channel_id) values (?, ?)", (guild_id, ch_id, ))
-        conn.commit()
-        await ctx.send(f"{ctx.author.mention}-> グローバルチャットに登録しました。")
+        if message.author.bot:
+            return
 
-        with open("all_data_arknights_main.db", "rb") as fc:
-            dbx.files_upload(fc.read(), "/all_data_arknights_main.db", mode=dropbox.files.WriteMode.overwrite)
+        date = datetime.datetime.now()
+        filename = f"{date.year}{date.month}{date.day}-{date.hour}{date.minute}{date.second}"
+        # 画像保存名(基本)を｢年月日-時分秒｣とする。
 
-        embed = discord.Embed(title="グローバルチャット[登録]", description=None, color=discord.Color.blue())
-        embed.add_field(name=f"GUILD", value=f"{ctx.guild.name}", inline=False)
-        embed.add_field(name="GUILD ID", value=f"{ctx.guild.id}", inline=False)
-        embed.add_field(name="CHANNEL", value=f"{ctx.channel.name}", inline=False)
-        embed.add_field(name="CHANNEL ID", value=f"{ctx.channel.id}", inline=False)
-        channel = self.bot.get_channel(739387932061859911)
-        await channel.send(embed=embed)
+        GLOBAL_CH_ID = []
+        for row in c.execute("SELECT channel_id FROM global_chat"):
+            GLOBAL_CH_ID.append(row[0])
 
-    @commands.command(name="del_global")
-    @commands.has_permissions(manage_guild=True)
-    async def _del_global(self, ctx):
-        ch_id = int(ctx.channel.id)
-        c.execute('DELETE FROM global_chat WHERE channel_id = ?', (ch_id, ))
-        conn.commit()
+        if message.channel.id in GLOBAL_CH_ID:
+            # 発言チャンネルidがGLOBAL_CH_IDに入っていたら反応
 
-        await ctx.send(f"{ctx.author.mention}-> グローバルチャットの登録を解除しました。")
+            if message.content.startswith(";" or ";add_global"):
+                return
+            # 発言時、頭に｢;｣がついていたらpass
+            if message.author.id not in admin_list:
+                conn_r = r.connect()
+                pp = conn_r.get("maintenance")
+                q = ['0', '1']
 
-        with open("all_data_arknights_main.db", "rb") as fc:
-            dbx.files_upload(fc.read(), "/all_data_arknights_main.db", mode=dropbox.files.WriteMode.overwrite)
+                if pp not in q:
+                    return await message.channel.send("現在、メンテナンス中です")
 
-        embed = discord.Embed(title="グローバルチャット[解除]", description=None, color=discord.Color.dark_red())
-        embed.add_field(name=f"GUILD", value=f"{ctx.guild.name}", inline=False)
-        embed.add_field(name="GUILD ID", value=f"{ctx.guild.id}", inline=False)
-        embed.add_field(name="CHANNEL", value=f"{ctx.channel.name}", inline=False)
-        embed.add_field(name="CHANNEL ID", value=f"{ctx.channel.id}", inline=False)
-        channel = self.bot.get_channel(739387932061859911)
-        await channel.send(embed=embed)
+            if message.content in ng_content:
+                return await message.delete()
+
+            channels = self.bot.get_all_channels()
+            # ボットの参加する全てのチャンネル取得
+            global_channels = [ch for ch in channels if ch.id in GLOBAL_CH_ID]
+            # channelsからGLOBAL_CH_IDと合致する物をglobal_channelsに格納
+            au = message.author.avatar_url
+            if ".gif" in str(au):
+                kakutyo = "gif"
+            else:
+                kakutyo = "png"
+            if message.attachments:
+                dcount = 0  # dcountには数字
+                for p in message.attachments:
+                    dcount += 1
+                    if ".gif" in p.filename:
+                        await p.save(f"{dcount}.gif")
+                    elif ".jpg" in p.filename:
+                        await p.save(f"{dcount}.jpg")
+                    elif ".png" in p.filename:
+                        await p.save(f"{dcount}.png")
+                    elif ".mp4" in p.filename:
+                        await p.save(f"{dcount}.mp4")
+                    elif ".mp3" in p.filename:
+                        await p.save(f"{dcount}.mp3")
+
+            for channel in global_channels:
+                # global_channelsから一つずつ取得
+
+                ch_webhooks = await channel.webhooks()
+                # channelのウェブフックを確認
+                webhook = discord.utils.get(ch_webhooks, name=GLOBAL_WEBHOOK_NAME)
+                # ch_webhooksからGLOBAL_WEBHOOK_NAMEの物を取得
+
+                if webhook is None:
+                    await channel.create_webhook(name=GLOBAL_WEBHOOK_NAME)
+                    # ウェブフックが無ければ作成後、処理は続ける
+                    ch_webhooks = await channel.webhooks()
+                    # channelのウェブフックを確認
+                    webhook = discord.utils.get(ch_webhooks, name=GLOBAL_WEBHOOK_NAME)
+                    # ch_webhooksからGLOBAL_WEBHOOK_NAMEの物を取得            
+                
+                if message.attachments:
+                    # 画像処理
+                    if channel.id == message.channel.id:
+                        continue
+
+                    # 送信チャンネルが発言チャンネルと同じなら次のループに
+
+                    if message.content:
+                        await webhook.send(content=message.content, username=message.author.name,
+                                            avatar_url=message.author.avatar_url_as(format=kakutyo))
+
+                    dcount = 0  # dcountには数字
+                    for p in message.attachments:
+                        dcount += 1
+                        if ".gif" in p.filename:
+                            filenames = f"{dcount}.gif"
+                        elif ".jpg" in p.filename:
+                            filenames = f"{dcount}.jpg"
+                        elif ".png" in p.filename:
+                            filenames = f"{dcount}.png"
+                        elif ".mp4" in p.filename:
+                            filenames = f"{dcount}.mp4"
+                        elif ".mp3" in p.filename:
+                            filenames = f"{dcount}.mp3"
+
+                        await webhook.send(file=discord.File(filenames), username=message.author.name,
+                                            avatar_url=message.author.avatar_url_as(format=kakutyo))
+
+                else:
+                    if channel.id == message.channel.id:
+                        continue
+
+                    await webhook.send(content=message.content, username=message.author.name,
+                                       avatar_url=message.author.avatar_url_as(format=kakutyo))
+            
+            user = await self.bot.fetch_user(self_id)
+            await message.add_reaction('\U00002705')
+            await asyncio.sleep(1)
+            await message.remove_reaction('\U00002705',user)
         
 def setup(bot):
     bot.add_cog(on_global_chat(bot))
