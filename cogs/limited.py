f94881ad66d22b021cf910f2ab308fadb9d4dc8b
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-09-09 11:51:37 +0900

    Update limited.py

--- cogs/limited.py
+++ cogs/limited.py
@@ -12,13 +12,13 @@ class limited(commands.Cog):
     async def on_member_join(self, member):
         if member.guild.id == 689263691669176426:
 
+            ms=len(member.guild.members)
+            await member.guild.edit(name=f'ドクター達の集いの場+{ms}人')
+
             if member.bot:
                 return
 
             await self.bot.get_channel(748752955712208966).send(f"{member}さんが参加しました！")
-
-            ms=len(member.guild.members)
-            await member.guild.edit(name=f'ドクター達の集いの場+{ms}人')
 
             em = discord.Embed(title="参加", color=discord.Color.blue())
             em.add_field(name="ユーザー名", value=f"{member}", inline=True)
@@ -30,10 +30,13 @@ class limited(commands.Cog):
     async def on_member_remove(self, member):
         if member.guild.id == 689263691669176426:
 
+            ms=len(member.guild.members)
+            await member.guild.edit(name=f'ドクター達の集いの場+{ms}人')
+
             if member.bot:
                 return
 
-            em = discord.Embed(title="脱退", color=discord.Color.purple())
+            em = discord.Embed(title="退出", color=discord.Color.purple())
             em.add_field(name="ユーザー名", value=f"{member}", inline=True)
             em.add_field(name='\u200b', value='\u200b')
             em.add_field(name="ユーザーID", value=f"{member.id}", inline=True)
